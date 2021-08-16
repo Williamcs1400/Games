27f62c51b7202dc0f18f962d1f6625532cc97f98
wcoelho <wcoelho@esec.com.br>
2021-08-16 20:08:35 -0300

    Iniciando projeto

---
+++ Game.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include "Game.h"
+
+using namespace std;
+
+int main(){
+    int a;
+    cout << "Game cpp" << endl;
+
+    return 0;
+}
